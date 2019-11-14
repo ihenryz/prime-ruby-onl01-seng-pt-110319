9c1fdc80e8508cd1952322c71fe2738eb54b9065
Henry Phan <h.phan06@gmail.com>
2019-11-14 19:14:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,8 @@ def prime?(number)
   if number > 1
     range = (start..number-1).to_a
     range.none? do |num_to_test|
-      # binding.pry
-      #none of the numbers should return true for none? to return true. If none of the numbers
-      # return true, then they are not divisible by the number and the number is prime
       number % num_to_test == 0
+      binding.pry
     end
   else
     false
